a1ae2ca36dfb04f8fa42ba7f2af1ac7b17751375
Alex <Alex@Surface>
2017-12-04 14:57:13 +0300

    problem of allienings is solved

--- Zharam/Form1.cs
+++ Zharam/Form1.cs
@@ -17,10 +17,17 @@ namespace Zharam
         {
             InitializeComponent();
             this.ChatList.Columns.Add("", ChatList.Width - 25);
-
+            ChatList.OwnerDraw = true;
+            ChatList.DrawSubItem += (sender, e) =>
+             {
+                 if (e.ItemIndex % 2 == 0)
+                     e.DrawText(TextFormatFlags.Right);
+                 else e.DrawText(TextFormatFlags.Left);
+                 
+             };
             for (int k = 0; k < 100; k++)
             {
-                ListViewItem listViewItem = new ListViewItem($"Sayjhzdvfcsdhjbasj,dfbsdv,ajdfn {k}");
+                ListViewItem listViewItem = new ListViewItem($"Say {k}");
                 ChatList.Items.Add(listViewItem);
             }
             ChatList.ItemSelectionChanged += ChatList_ItemSelectionChanged;
