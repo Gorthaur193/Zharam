268c8af9941302451581249689dc9e8e65cc8f48
Alex <Alex@Surface>
2017-12-07 08:46:28 +0300

    Undrestood how websocket server should be created

---
+++ ZharamServ/Controllers/MessageController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ZharamServ.Controllers
+{
+    public class MessageController : ApiController
+    {
+        // GET: api/Message
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST: api/Message
+        public string Post(string value)
+        {
+            return (value + '1');
+        }
+    }
+}
---
+++ ZharamServ/ws.ashx.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Web.WebSockets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZharamServ
+{
+    /// <summary>
+    /// Summary description for ws
+    /// </summary>
+    public class Ws : IHttpHandler
+    {
+        public static WebSocketCollection clients = new WebSocketCollection();
+
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context.IsWebSocketRequest)
+                context.AcceptWebSocketRequest(new MyWebSocketHandler());
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+
+    public class MyWebSocketHandler : WebSocketHandler
+    {
+        private string name;
+
+        public MyWebSocketHandler()
+        {
+        }
+
+        public override void OnOpen()
+        {
+            this.name = this.WebSocketContext.QueryString["token"];
+            Ws.clients.Add(this);
+            Ws.clients.Broadcast(name + " has connected.");
+        }
+
+        public override void OnMessage(string message)
+        {
+            Ws.clients.Broadcast(string.Format("{0} said: {1}", name, message));
+        }
+
+        public override void OnClose()
+        {
+            Ws.clients.Remove(this);
+            Ws.clients.Broadcast(string.Format("{0} has gone away.", name));
+        }
+    }
+
+}
