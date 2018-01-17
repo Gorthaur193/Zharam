56b5ba861db72358cdc36aa440af42ec370ae07b
Alex <Alex@Surface>
2018-01-17 16:12:56 +0300

    beginning with database

---
+++ ZharamServ/LogicObjects/Room.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.ServiceModel.WebSockets;
+using Microsoft.Web.WebSockets;
+
+namespace ZharamServ
+{
+    public class Room
+    {
+        public List<User> UserList { get; private set; }
+
+        public void Broadcast(string message)
+        {
+            foreach (var item in UserList)
+                item.UserConnection.Send(message);
+        }
+
+    }
+}
---
+++ ZharamServ/LogicObjects/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZharamServ
+{
+    public class User
+    {
+        public ModifiedWebSocketHandler UserConnection { get; }
+        public string Name { get; }
+        public string CurrentToken { get; }
+        public string Id { get; }
+        public Room CurrentRoom { get; private set; }
+        public void SwitchRoom(Room room) => CurrentRoom = room;
+
+        public User(string jsonInfo)
+        {
+            
+        }
+    }       
+}
